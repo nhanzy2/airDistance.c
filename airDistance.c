a5dabae6f4b42c643292dd562381bbc531b8ef25
Bsudbeck <69834675+Bsudbeck@users.noreply.github.com>
2020-08-30 20:55:44 -0500

    Update airDistance.c

--- airDistance.c
+++ airDistance.c
@@ -19,16 +19,16 @@
  double pi = 4.0 * atan(1.0);
 
  printf("Enter Lat1:");
- scanf("%lf", double&Lat1);
+ scanf("%lf", &Lat1);
 
  printf("Enter Lon1:");
- scanf("%lf" double&Lon1);
+ scanf("%lf" &Lon1);
 
  printf("Enter Lat2:");
- scanf("%lf" double&Lat2);
+ scanf("%lf" &Lat2);
 
  printf("Enter Lon2:");
- scanf("%lf" double&Lon2);
+ scanf("%lf" &Lon2);
   
   Lat1 = Lat1 / 180/pi;
   Lat2 = Lat2 / 180/pi;
